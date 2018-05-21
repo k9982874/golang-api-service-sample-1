48cd12771a5103e98b657b8a8fa6f8f9e4a399ad
kevin zhou <kevin.zhou@imaginato.com>
2018-05-21 14:00:11 +0800

    initialize commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	controllers "github.com/k9982874/golang-api-service-sample/controllers"
+)
+
+func main() {
+	r := gin.Default()
+
+	controllers.NewApplicationController(r)
+
+	r.Run(":9999")
+}
